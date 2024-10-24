365166440b49ecb7a5d91219dab30df3f9dca14f
alexandra <alexandra.ding.mm@gmail.com>
2024-10-24 19:57:04 -0400

    update auton.java

--- src/main/java/frc/robot/commands/Auton.java
+++ src/main/java/frc/robot/commands/Auton.java
@@ -1,5 +1,11 @@
 package frc.robot.commands;
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class Auton {
-
+public class Auton extends SequentialCommandGroup{
+    public Auton(Drivetrain drivetrain){
+        addCommands(
+            new DriveDistance(1.5,3,drivetrain)
+        );
+    }
 }
